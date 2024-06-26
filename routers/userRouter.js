9d82c9941b8e7df3d328fb611fe3a8df2df23948
adar7295 <ssrivastav7295@gmail.com>
2024-06-26 15:53:28 +0530

    delete

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -54,8 +54,14 @@ router.get('/update', (req, res) => {
 });
 
 // delete
-router.get('/delete', (req, res) => {
-    res.send('response from user delete');
+router.get('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err)
+    });
 });
 
 module.exports = router;
